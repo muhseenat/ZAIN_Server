6e49a1c13808d4332aa0a9e70ff7d15437a5f4af
Muhseena T <muhseenat@protonmail.com>
2022-02-20 16:04:05 +0530

    Sales Report bug fixed

--- controllers/order.js
+++ controllers/order.js
@@ -1,12 +1,13 @@
 const User = require("../models/User");
 const Order = require("../models/Order");
-const Coupon = require ("../models/Coupon")
+const Coupon = require("../models/Coupon");
 const Cart = require("../models/Cart");
 const mongoose = require("mongoose");
 const Razorpay = require("razorpay");
 const shortid = require("shortid");
 const objectId = mongoose.Types.ObjectId;
 const crypto = require("crypto");
+const { log } = require("console");
 
 const razorpay = new Razorpay({
   key_id: process.env.RAZORPAY,
@@ -90,7 +91,7 @@ const addAddress = async (req, res) => {
                 const options = {
                   amount: amount * 100,
                   currency: "INR",
-                  receipt: "" + orderId, 
+                  receipt: "" + orderId,
                   payment_capture: 1,
                 };
                 console.log(options);
@@ -164,17 +165,19 @@ const verifyPayment = (req, res) => {
     res.status(401);
   }
 };
-//GET ADDRESS API 
+//GET ADDRESS API
 const getAddress = (req, res) => {
   const userId = req.params.id;
   console.log(userId);
- 
-    User.findOne({_id:objectId(userId)},{address:1}).then((adr)=>{
-      res.status(200).json({ address: adr.address})
-    }).catch((err)=>{
-      console.log(err);
-      res.status(400)
+
+  User.findOne({ _id: objectId(userId) }, { address: 1 })
+    .then((adr) => {
+      res.status(200).json({ address: adr.address });
     })
+    .catch((err) => {
+      console.log(err);
+      res.status(400);
+    });
 };
 //GET ORDER API
 const getOrders = (req, res) => {
@@ -212,71 +215,81 @@ const changeStatus = async (req, res) => {
 };
 
 //GET ORDER HISTORY API
- const orderHistory=async(req,res)=>{
- const id= req.params.id
- console.log(id);
- await Order.aggregate([
-  {
-    $match:{userId:id}
-  },{
-    $unwind:"$products"
-  },
-]).then((order)=>{
-  console.log(order);
-  res.status(200).json({order})
-}).catch((err)=>{
-  res.staus(400).json({err})
-})
- }
- //APPLY COUPON API
- const applyCoupon= async(req,res)=>{
-   const {userId,couponCode} = req.body
-   console.log(req.body);
-   let checkCoupon=  await Coupon.findOne({code:couponCode})
-   console.log(checkCoupon);
-   
-   if(checkCoupon){
-     let userExist= checkCoupon.userId.findIndex( users => users==userId)
-     console.log(userExist);
-     if(userExist==-1){
-       Coupon.updateOne({code:couponCode},{
-        $push:{userId:userId} 
-       }).then((resp)=>{
-         res.status(200).json({checkCoupon})
-       }).catch((err)=>{
-         console.log(err);
-         res.status(400)
-       })
-     }else{
-       res.status(400).json({errorMessage:"Coupon already applied"})
-     }
-    res.status(200).json({checkCoupon})
-
-   }else{
-     res.status(400).json({errorMessage:"Invalid Coupon"})
-   }
- }
-
-
- const salesReport=()=>{
-  Order.aggregate([
-    
-    {$unwind:"$products"},
-    {$match:{"product.status":"delivered"}}
-    
-    
-    ])
- }
-
-
- Order.aggregate([
-    
-  {$unwind:"$products"},
-  {$match:{"product.status":"delivered",date:{$gte:new Date("10/02/2022"),$lte:new Date("15/02/22")}}}
-  
-  
+const orderHistory = async (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  await Order.aggregate([
+    {
+      $match: { userId: id },
+    },
+    {
+      $unwind: "$products",
+    },
   ])
+    .then((order) => {
+      console.log(order);
+      res.status(200).json({ order });
+    })
+    .catch((err) => {
+      res.staus(400).json({ err });
+    });
+};
+//APPLY COUPON API
+const applyCoupon = async (req, res) => {
+  const { userId, couponCode } = req.body;
+  console.log(req.body);
+  let checkCoupon = await Coupon.findOne({ code: couponCode });
+  console.log(checkCoupon);
 
+  if (checkCoupon) {
+    let userExist = checkCoupon.userId.findIndex((users) => users == userId);
+    console.log(userExist);
+    if (userExist == -1) {
+      Coupon.updateOne(
+        { code: couponCode },
+        {
+          $push: { userId: userId },
+        }
+      )
+        .then((resp) => {
+          res.status(200).json({ checkCoupon });
+        })
+        .catch((err) => {
+          console.log(err);
+          res.status(400);
+        });
+    } else {
+      res.status(400).json({ errorMessage: "Coupon already applied" });
+    }
+    res.status(200).json({ checkCoupon });
+  } else {
+    res.status(400).json({ errorMessage: "Invalid Coupon" });
+  }
+};
+
+const salesReport = (req, res) => {
+  Order.aggregate([
+    { $unwind: "$products" },
+    { $match: { "products.status": "delivered" } },
+    {
+      $unwind: "products",
+    },
+  ])
+    .then((report) => {
+      console.log(report);
+      res.status(200).json({ report });
+    })
+    .catch((err) => {
+      res.status(400).json({ err });
+    });
+};
+
+//  Order.aggregate([
+
+//   {$unwind:"$products"},
+//   {$match:{"product.status":"delivered",date:{$gte:new Date("10/02/2022"),$lte:new Date("15/02/22")}}}
+
+//   ])
 
 module.exports = {
   addAddress,
@@ -286,5 +299,5 @@ module.exports = {
   changeStatus,
   verifyPayment,
   applyCoupon,
-  salesReport
+  salesReport,
 };
